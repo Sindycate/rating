4ce2b1563e10bed35c6ba00febcda24e2c52429d
Pavel <SmetaninPavel.rus@gmail.com>
2014-10-10 20:30:18 +0600

    v 0.1.2
    Fixed bugs

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once './php/index.php';
+require_once '/php/index.php';
 
 ?>
-ffd
--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../html/header.html';
-require_once '../html/main.html';
-require_once '../html/footer.html';
+require_once '/html/header.html';
+require_once '/html/main.html';
+require_once '/html/footer.html';
 ?>
