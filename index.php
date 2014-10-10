0b36db168bab57796a67f3f90cc5091431398b33
Pavel <SmetaninPavel.rus@gmail.com>
2014-10-10 21:54:26 +0600

    v 0.1.3
    Добавлена форма регистрации. Подключены таблицы стилей.

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+$requestURI    = explode('/', $_SERVER['REDIRECT_URL']); //['REQUEST_URI']);
+$requestURI[0] = (count($requestURI)-1);
 
-require_once '/php/index.php';
+if ($requestURI[2] == 'registration') {
+	require_once '/php/registration.php';
+} else {
+	require_once '/php/index.php';
+}
+
 
 ?>
--- php/registration.php
+++ php/registration.php
@@ -1,6 +1,6 @@
 <?php
 require_once '/html/header.html';
-require_once '/html/main.html';
+require_once '/html/registration.html';
 require_once '/html/footer.html';
 ?>
 
