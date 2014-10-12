7904406c9f84f82566032a8bd82ea3bc1b626adc
SergeShaw <SergeShaw@gmail.com>
2014-10-12 23:04:33 +0600

    v 0.2.5
    fix bugs
    общая проверка на авторизированность
    общий массив $data
    
    твой ход @SmetaninPavel :koala:

--- php/main.php
+++ php/main.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * Массив хранящий все флаги - ошибки/резульаты/успехи
- *
- * @var array
- **/
-$data = array();
-
-/**
  * Управляющая функция страницы
  *
  * @return void
@@ -15,73 +8,10 @@ $data = array();
  **/
 function main()
 {
-	global $data;
-
-	if (sign_in_check())
-	{
-		$data['user']['id'] = $_SESSION['user_id'];
-	}
+	;
 }
 
-/**
- * Autorization check
- *
- * @return bool
- * @author SergeShaw
- **/
-function sign_in_check()
-{
-	// на будущие проверка включённости куков
-	//
-	// if ($auto_cook_chek == true) // хуйня какая-то которая передавалась как предустановленный тру в функцию
-	// {
-	// 	$this->cookcheck();
-	// }
 
-	session_name('lowlogin');
-
-	if (isset($_COOKIE[session_name()]))
-	{
-		session_start();
-		return true;
-	}
-	else if (isset($_COOKIE['highlogin']))    // проверка на hight_login
-	{
-		try
-		{
-			$userHash = md5($_SERVER['HTTP_USER_AGENT'] . $_COOKIE['highlogin']);
-
-			$db_hash = database_main::$DBH->prepare(
-				"SELECT `id`
-				 FROM `session`
-				 WHERE `hash` = :userHash");
-			$db_hash->bindValue(':userHash', $userHash);
-			$db_hash->execute();
-
-			if ($db_hash->rowCount() == 1)
-			{
-				session_start();
-
-				$row = $db_hash->fetch();
-				$_SESSION['user_id'] = $row['id'];
-
-				return true;
-			}
-			else
-			{
-				setcookie('highlogin', "", time() - (60 * 60 * 24 * 100), '/rating'); // удаляем
-
-				return false;
-			}
-		}
-		catch(PDOException $ee)
-		{
-			$data['error']['PDO'] = "Ошибка базы данных: " . $ee->getMessage();
-		}
-	}
-
-	return false;
-}
 
 main();
 
