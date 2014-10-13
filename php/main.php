35b7792c7c21bf4ee065df8a0671d06627ecc17e
SergeShaw <SergeShaw@gmail.com>
2014-10-13 21:23:17 +0600

    v 0.3.3
    список всех студентов учасвующих в рейтинге на главной

--- php/main.php
+++ php/main.php
@@ -8,7 +8,37 @@
  **/
 function main()
 {
-	;
+	global $data;
+
+	$data['students'] = get_all_students();
+}
+
+/**
+ * Выборка всех студентов из рейтинга
+ *
+ * @return array
+ * @author SergeShaw
+ **/
+function get_all_students()
+{
+	try
+	{
+		$db_get_students = database::$DBH->prepare(
+			"SELECT *
+			 FROM `students`
+			 ORDER BY `category` desc");
+		$db_get_students->execute();
+
+		if ($db_get_students->rowCount())
+		{
+			return $db_get_students->fetchAll();
+		}
+	}
+	catch (PDOExeption $ee)
+	{
+		$data['error']['PDO'] = "Ошибка базы данных: " . $e->getMessage();
+		return false;
+	}
 }
 
 
